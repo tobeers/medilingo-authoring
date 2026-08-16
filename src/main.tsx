50eb49803a33ac03b60df43ac5ee8ef3fd0b5153
tobeers <t.heissenberger0@gmail.com>
2026-08-16 18:13:21 +0200

    Expose device-test publish workflow

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import DeviceTestPublish from './DeviceTestPublish';
 import './styles.css';
 import './workbench.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <DeviceTestPublish />
   </React.StrictMode>,
 );
