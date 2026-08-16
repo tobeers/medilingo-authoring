9ad21fd0b07e30a7fbb3130347931339ff9c641d
tobeers <t.heissenberger0@gmail.com>
2026-08-16 18:21:40 +0200

    Wire unified content studio into workbench

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import StudioWrapper from './StudioWrapper';
 import DeviceTestPublish from './DeviceTestPublish';
 import './styles.css';
 import './workbench.css';
+import './studio.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <StudioWrapper />
     <DeviceTestPublish />
   </React.StrictMode>,
 );
